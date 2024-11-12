6686aa5ee5ce76ca9cdbabed6822f810c34833e1
Harsh Dabas <iamharshdabas@gmail.com>
2024-11-12 17:48:35 +0530

    chore(linux): improve readability

--- linux/install.sh
+++ linux/install.sh
@@ -3,48 +3,56 @@
 # Update system
 paru
 
-# Install packages
-sudo pacman -S ttf-firacode-nerd github-cli fd fzf lazygit unzip npm telegram-desktop neovide interception-caps2esc
-paru zen-browser-bin
+# Install necessary packages
+sudo pacman -S --needed \
+  fd \
+  fzf \
+  github-cli \
+  interception-caps2esc \
+  lazygit \
+  neovide \
+  npm \
+  telegram-desktop \
+  ttf-firacode-nerd \
+  unzip
 
-# Git
+# Install additional packages using paru
+paru -S --needed zen-browser-bin
 
-## First auth Github Cli
+# Git configuration
 
+## Authenticate GitHub CLI
 gh auth login
 
-### for [octo.nvim](https://github.com/pwntester/octo.nvim)
-
+## Refresh GitHub CLI token for octo.nvim
 gh auth refresh -s read:project
 
-## Get all of your Repos
-
-mkdir Repos
+## Clone all repositories from GitHub user
+mkdir -p Repos
 cd Repos || exit
 gh repo list iamharshdabas --json nameWithOwner --jq '.[].nameWithOwner' | xargs -I {} gh repo clone {}
 cd ~ || exit
 
-## Set up git
-
+## Set global Git configuration
 git config --global user.email "iamharshdabas@gmail.com"
 git config --global user.name "Harsh Dabas"
 git config --global core.editor "nvim"
 git config --global init.defaultBranch main
 
-# Setup config
+# Setup configuration files
 
-# nvim symlink
+## Create symlink for nvim configuration
 ln -s ~/Repos/nvim/ ~/.config/nvim
 
-# kitty
-rm .config/kitty/kitty.conf
-cp Repos/stuff/linux/kitty/* .config/kitty/
+## Copy kitty configuration files
+rm -f ~/.config/kitty/kitty.conf
+cp Repos/stuff/linux/kitty/* ~/.config/kitty/
 
-# hypr
-rm .config/hypr/userprefs.conf .config/hypr/keybindings.conf .config/hypr/monitors.conf
-cp Repos/stuff/linux/hypr/* .config/hypr/
+## Copy hypr configuration files
+rm -f ~/.config/hypr/keybindings.conf ~/.config/hypr/monitors.conf ~/.config/hypr/userprefs.conf
+cp Repos/stuff/linux/hypr/* ~/.config/hypr/
 
-# caps2esc
+## Setup caps2esc
 sudo cp Repos/stuff/linux/caps2esc/udevmon.yaml /etc/
 sudo cp Repos/stuff/linux/caps2esc/udevmon.service /etc/systemd/system/
 sudo systemctl enable --now udevmon.service
